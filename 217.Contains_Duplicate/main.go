b42feb587ecaafd3771ffcdda95fe956678f3f1c
Faroukhamadi <faroukhamadi1@outlook.com>
2022-06-17 15:54:39 +0100

    add optimal solution

---
+++ 217.Contains_Duplicate/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+// O(n) time complexity
+func containsDuplicate(nums []int) bool {
+	hashSet := make(map[int]bool)
+	for index, item := range nums {
+		fmt.Printf("index: %d\n", index)
+		if hashSet[item] {
+			return true
+		} else {
+			hashSet[item] = true
+		}
+	}
+	return false
+}
+
+func main() {
+	nums := []int{1, 2, 3, 6}
+	fmt.Println(containsDuplicate(nums))
+}
