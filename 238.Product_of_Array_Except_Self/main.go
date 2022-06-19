b24d07e2e5a2da8decec8f45e1b35a799da1e04b
Faroukhamadi <faroukhamadi1@outlook.com>
2022-06-20 00:34:04 +0100

    add sol238

---
+++ 238.Product_of_Array_Except_Self/main.go
@@ -0,0 +1,27 @@
+package main
+
+func productExceptSelf(nums []int) []int {
+	res := make([]int, len(nums))
+
+	for i := range nums {
+		res[i] = 1
+	}
+
+	prefix := 1
+	for i := 0; i < len(nums); i++ {
+		res[i] = prefix
+		prefix *= nums[i]
+	}
+
+	postfix := 1
+	for i := len(nums) - 1; i >= 0; i-- {
+		res[i] *= postfix
+		postfix *= nums[i]
+	}
+
+	return res
+
+}
+func main() {
+
+}
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("go pls sucks")
+	fmt.Println("go please doesn't actually suck")
 }
