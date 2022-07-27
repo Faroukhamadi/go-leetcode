3d458bd0eab4282e343811e87927c0477485639a
Faroukhamadi <faroukhamadi1@outlook.com>
2022-07-27 07:01:39 +0100

    add sol for 235

---
+++ 235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree/main.go
@@ -0,0 +1,28 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	for root != nil {
+
+		if p == root || q == root {
+			return root
+		}
+
+		if (p.Val < root.Val && q.Val > root.Val) || (p.Val > root.Val && q.Val < root.Val) {
+			return root
+		}
+
+		if p.Val < root.Val && q.Val < root.Val {
+			root = root.Left
+		} else {
+			root = root.Right
+		}
+	}
+
+	return nil
+}
