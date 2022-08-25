c6e8fde01b7aeb5859a05424010ff01bc1db43ee
Faroukhamadi <faroukhamadi1@outlook.com>
2022-08-25 17:35:17 +0100

    add sol for 202

---
+++ 202.Happy_Number/main.go
@@ -0,0 +1,26 @@
+package main
+
+func isHappy(n int) bool {
+	switch n {
+	case 1, 7:
+		return true
+	case 0, 2, 3, 4, 5, 6, 8, 9:
+		return false
+	default:
+		return isHappy(sumNum(n))
+	}
+}
+
+func sumNum(n int) int {
+	out, last := 0, 0
+	for n > 0 {
+		last = n % 10
+		out += last * last
+		n /= 10
+	}
+	return out
+}
+
+func main() {
+
+}
