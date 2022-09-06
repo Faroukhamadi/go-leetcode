b3ab57e0742d07fbae43a31f4f3635fa0d92473d
Faroukhamadi <faroukhamadi1@outlook.com>
2022-09-06 19:15:48 +0100

    add sol for 198

---
+++ 198.House_Robber/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func rob(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	if len(nums) == 2 {
+		return max(nums[0], nums[1])
+	}
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	dp[1] = max(nums[0], nums[1])
+	for i := 2; i < len(dp); i++ {
+		dp[i] = max(nums[i]+dp[i-2], dp[i-1])
+	}
+	return dp[len(dp)-1]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	fmt.Println(rob([]int{1, 2, 3, 1}))
+}
